29ca236c52f86a12a93ae24f82e607a1dce71481
gyojincompany <luckyred@gmail.com>
2022-11-03 10:58:56 +0900

    spring transaction 연습

---
+++ src/main/java/com/gyojincompany/study22/TicketDao.java
@@ -0,0 +1,36 @@
+package com.gyojincompany.study22;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.PreparedStatementCreator;
+
+public class TicketDao {
+	
+	JdbcTemplate template;
+
+	public void setTemplate(JdbcTemplate template) {
+		this.template = template;
+	}
+	
+	public void buyTicket(final TicketDto dto) {
+		
+		this.template.update(new PreparedStatementCreator() {
+			
+			@Override
+			public PreparedStatement createPreparedStatement(Connection con) throws SQLException {
+				// TODO Auto-generated method stub
+				String sql = "insert into card (consumerid,amount) values (?,?)";
+				PreparedStatement pstmt = con.prepareStatement(sql);
+				pstmt.setString(1, dto.getCosumerid());
+				pstmt.setString(2, dto.getAmount());
+				
+				return pstmt;
+			}
+		});
+		
+	}
+	
+}
---
+++ src/main/java/com/gyojincompany/study22/TicketDto.java
@@ -0,0 +1,23 @@
+package com.gyojincompany.study22;
+
+public class TicketDto {
+	
+	private String consumerid;
+	private String amount;
+	
+	public String getCosumerid() {
+		return consumerid;
+	}
+	public void setCosumerid(String cosumerid) {
+		this.consumerid = cosumerid;
+	}
+	public String getAmount() {
+		return amount;
+	}
+	public void setAmount(String amount) {
+		this.amount = amount;
+	}
+	
+	
+	
+}
